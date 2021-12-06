0be75b4641c449438c8eefd35cbd8059bf55062d
Andrew Smith <andrewts129@gmail.com>
2021-12-06 14:23:50 -0500

    Day 6, part 2

--- src/main/scala/Day6.scala
+++ src/main/scala/Day6.scala
@@ -2,38 +2,54 @@ package io.andrewsmith.advent_of_code_2021
 
 import Util.readLines
 
-import scala.annotation.tailrec
-
 object Day6 {
-  case class Fish(numDaysUntilReproduction: Int) {
-    def tomorrow: Seq[Fish] = {
-      numDaysUntilReproduction match {
-        case 0 => Seq(Fish(6), Fish(8))
-        case _ => Seq(Fish(numDaysUntilReproduction - 1))
+  case class School(fishCounts: Map[Int, Long]) {
+    def fishAfterNDays(n: Int): Long = {
+      if (n == 0) {
+        fishCounts.values.sum
+      } else {
+        tomorrow.fishAfterNDays(n - 1)
       }
+    }
+
+    def tomorrow: School = {
+      // The compiler is being very strange here
+      val huh: Long = fishCounts.getOrElse(7, 0)
+      val what: Long = fishCounts.getOrElse(0, 0)
+      val hmm: Long = huh + what
+      val newCounts: Map[Int, Long] = Map(
+        0 -> fishCounts.getOrElse(1, 0),
+        1 -> fishCounts.getOrElse(2, 0),
+        2 -> fishCounts.getOrElse(3, 0),
+        3 -> fishCounts.getOrElse(4, 0),
+        4 -> fishCounts.getOrElse(5, 0),
+        5 -> fishCounts.getOrElse(6, 0),
+        6 -> hmm,
+        7 -> fishCounts.getOrElse(8, 0),
+        8 -> what
+      )
+
+      School(newCounts)
     }
   }
 
-  object Fish {
-    def parseMany(string: String): Seq[Fish] = {
-      string.split(",").map(Fish.parse)
-    }
+  object School {
+    def parse(lines: Seq[String]): School = {
+      val startingFish = lines.flatMap(_.split(",")).map(_.toInt)
 
-    def parse(string: String): Fish = {
-      Fish(string.toInt)
+      School(
+        startingFish.groupBy(identity).map {
+          case (daysTilReproduction, fish) => daysTilReproduction -> fish.size
+        }
+      )
     }
   }
 
   def numberOfFishAfter80Days(fileName: String): Long = {
-    numberOfFishAfterNDays(readLines(fileName).flatMap(Fish.parseMany), 80)
+    School.parse(readLines(fileName)).fishAfterNDays(80)
   }
 
-  @tailrec
-  private def numberOfFishAfterNDays(fish: Seq[Fish], n: Int): Long = {
-    if (n == 0) {
-      fish.size
-    } else {
-      numberOfFishAfterNDays(fish.flatMap(_.tomorrow), n - 1)
-    }
+  def numberOfFishAfter256Days(fileName: String): Long = {
+    School.parse(readLines(fileName)).fishAfterNDays(256)
   }
 }
--- src/test/scala/Day6Spec.scala
+++ src/test/scala/Day6Spec.scala
@@ -22,4 +22,24 @@ class Day6Spec extends AnyFunSpec {
       }
     }
   }
+
+  describe("#numberOfFishAfter256Days") {
+    describe("sample input") {
+      val input = "input/day6_sample.txt"
+      val expectedOutput = 26984457539L
+
+      it("returns the correct result") {
+        assert(Day6.numberOfFishAfter256Days(input) == expectedOutput)
+      }
+    }
+
+    describe("real input") {
+      val input = "input/day6.txt"
+      val expectedOutput = -1
+
+      it("returns the correct result") {
+        assert(Day6.numberOfFishAfter256Days(input) == expectedOutput)
+      }
+    }
+  }
 }
