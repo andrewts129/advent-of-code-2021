76352d43fa0a57fa5c36ae2b07b40973d7b7b5ea
Andrew Smith <andrewts129@gmail.com>
2021-12-13 09:43:11 -0500

    Day 13, part 2

--- src/main/scala/Day13.scala
+++ src/main/scala/Day13.scala
@@ -14,6 +14,9 @@ object Day13 {
   }
 
   case class Paper(points: Set[Point]) {
+    private val width = points.map(_.x).max
+    private val height = points.map(_.y).max
+
     def apply(instruction: Instruction): Paper = {
       instruction match {
         case HorizontalFold(crease) => apply(
@@ -25,12 +28,36 @@ object Day13 {
       }
     }
 
+    def apply(instructions: Seq[Instruction]): Paper = {
+      if (instructions.isEmpty) {
+        this
+      } else {
+        this.apply(instructions.head).apply(instructions.tail)
+      }
+    }
+
+    def print(): Unit = {
+      println(representation)
+    }
+
     private def apply(func: Point => Point): Paper = {
       Paper(points.map(func))
     }
 
     def numVisibleDots: Int = {
       points.size
+    }
+
+    private def representation: String = {
+      (0 to height).map {
+        y => (0 to width).map {
+          x => if (pointExists(x, y)) "*" else "."
+        }.mkString
+      }.mkString("\n")
+    }
+
+    private def pointExists(x: Int, y: Int): Boolean = {
+      points.contains(Point(x, y))
     }
   }
 
@@ -56,6 +83,12 @@ object Day13 {
     val (paper, instructions) = parse(fileName)
 
     paper.apply(instructions.head).numVisibleDots
+  }
+
+  def printActivationCode(fileName: String): Unit = {
+    val (paper, instructions) = parse(fileName)
+
+    paper.apply(instructions).print()
   }
 
   private def parse(fileName: String): (Paper, Seq[Instruction]) = {
--- src/test/scala/Day13Spec.scala
+++ src/test/scala/Day13Spec.scala
@@ -2,6 +2,8 @@ package io.andrewsmith.advent_of_code_2021
 
 import org.scalatest.funspec._
 
+import java.io.ByteArrayOutputStream
+
 class Day13Spec extends AnyFunSpec {
   describe("#dotsVisibleAfterFirstFold") {
     describe("sample input") {
@@ -19,6 +21,49 @@ class Day13Spec extends AnyFunSpec {
 
       it("returns the correct result") {
         assert(Day13.dotsVisibleAfterFirstFold(input) == expectedOutput)
+      }
+    }
+  }
+
+  describe("#printActivationCode") {
+    describe("sample input") {
+      val input = "input/day13_sample.txt"
+      val expectedOutput = """*****
+                             |*...*
+                             |*...*
+                             |*...*
+                             |*****
+                             |""".stripMargin
+
+      it("returns the correct result") {
+        val output = new ByteArrayOutputStream()
+
+        Console.withOut(output) {
+          Day13.printActivationCode(input)
+        }
+
+        assert(output.toString == expectedOutput)
+      }
+    }
+
+    describe("real input") {
+      val input = "input/day13.txt"
+      val expectedOutput = """****..**..*..*.*..*.***..****..**..***.
+                             |*....*..*.*..*.*.*..*..*.*....*..*.*..*
+                             |***..*..*.****.**...*..*.***..*....*..*
+                             |*....****.*..*.*.*..***..*....*....***.
+                             |*....*..*.*..*.*.*..*.*..*....*..*.*...
+                             |****.*..*.*..*.*..*.*..*.****..**..*...
+                             |""".stripMargin
+
+      it("returns the correct result") {
+        val output = new ByteArrayOutputStream()
+
+        Console.withOut(output) {
+          Day13.printActivationCode(input)
+        }
+
+        assert(output.toString == expectedOutput)
       }
     }
   }
